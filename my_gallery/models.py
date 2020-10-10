a95c0e13cc201634366efa86f58bae65b9292bc4
Vector254 <ochrist7@gmail.com>
2020-10-10 13:24:03 +0300

    create models

--- my_gallery/models.py
+++ my_gallery/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to = '', null = True, blank = True)
+    name = models.CharField(max_length=20)	   
+    description = models.CharField(max_length=100)	    
+    category = models.ForeignKey('Category', on_delete = models.CASCADE, null='True', blank=True)	    
+    location = models.ForeignKey('Location', on_delete = models.CASCADE, null='True', blank=True)
+    
+
+class Category(models.Model):
+    name = models.CharField(max_length=15,) 	    
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=20,) 
