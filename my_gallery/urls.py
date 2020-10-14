5ddff13872cdd42964e468ed0d73b54e515d12eb
Vector254 <ochrist7@gmail.com>
2020-10-14 08:24:34 +0300

    update requirements

--- my_gallery/urls.py
+++ my_gallery/urls.py
@@ -12,5 +12,3 @@ urlpatterns=[
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT),
-if not settings.DEBUG:
-    urlpatterns += [ url(r'^uploads/(?P<path>.)$', serve,{'document_root': settings.MEDIA_ROOT}), url(r'^static/(?P<path>.)$', serve,{'document_root': settings.STATIC_ROOT}), ]
